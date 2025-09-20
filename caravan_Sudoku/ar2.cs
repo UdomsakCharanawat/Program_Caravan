57396d4c49520d6118102c6c2cb56e888193c783
UdomsakCharanawat <59986784+UdomsakCharanawat@users.noreply.github.com>
2025-09-20 16:39:57 +0700

    Add files via upload

---
+++ caravan_Sudoku/ar2.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ar2 : MonoBehaviour, ISelectHandler
+{
+    public bool select;
+    public Text _text;
+    public Button _button;
+    public bool lockValue, nullValue, inputValue, wrongValue, textValue;
+    public Color _lock, _input, _wrong, _textVal;
+    public Color[] _null;
+
+    public ColorBlock cb;
+
+    public bool easy, medium;
+
+
+    void Start()
+    {
+        cb = _button.colors;
+
+        if (lockValue)
+        {
+            //cb.normalColor = _lock;
+            //_button.colors = cb;
+            this.gameObject.GetComponent<Image>().color = _lock;
+            _button.interactable = false;
+        }
+        else if (nullValue)
+        {
+            //var ran = Random.Range(0, _null.Length);
+            //cb.normalColor = _null[ran];
+            //_button.colors = cb;
+            //_text.color = _textVal;
+
+            if (easy)
+            {
+                if (this.gameObject.tag == "1" || this.gameObject.tag == "4" || this.gameObject.tag == "5")
+                {
+                    cb.normalColor = _null[0];
+                    _button.colors = cb;
+                    _text.color = _textVal;
+                    //Debug.Log("tag : " + this.gameObject.tag);
+                }
+                else
+                {
+                    cb.normalColor = _null[1];
+                    _button.colors = cb;
+                    _text.color = _textVal;
+                }
+            }
+
+            if (medium)
+            {
+                if (this.gameObject.tag == "1" || this.gameObject.tag == "3" || this.gameObject.tag == "5" || this.gameObject.tag == "7" || this.gameObject.tag == "9")
+                {
+                    cb.normalColor = _null[0];
+                    _button.colors = cb;
+                    _text.color = _textVal;
+                    //Debug.Log("tag : " + this.gameObject.tag);
+                }
+                else
+                {
+                    cb.normalColor = _null[1];
+                    _button.colors = cb;
+                    _text.color = _textVal;
+                }
+            }
+            
+            //ฟังก์ชัน ISODD ส่งกลับค่า TRUE ถ้าจำนวนเป็นเลขคี่ หรือส่งกลับค่า FALSE
+            //var mod = int.Parse(this.gameObject.tag)%2;
+            //Debug.Log("tag : "+this.gameObject.tag + " : " + mod);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        //Debug.Log(this.gameObject.name);
+        Array2DEditor.ar _ar = gameObject.GetComponentInParent<Array2DEditor.ar>();
+        _ar.selectButton = this.gameObject.name;
+
+    }
+
+}
+
---
+++ caravan_Sudoku/keyboardKey.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class keyboardKey : MonoBehaviour
+{
+    //public bool easy, medium;
+    public int sizeEasy, sizeMedium;
+    private int sizeTemp;
+    Array2DEditor.ar _ar;
+    ar2 _ar2;
+    public int col, row;
+    public string tagSelect;
+    GameObject[] ertag;
+    public GameObject[] rowError, colError, tagError;
+
+    private void Start()
+    {
+        _ar = gameObject.GetComponentInParent<Array2DEditor.ar>();
+
+        resetError();
+    }
+
+    public void input(int value)
+    {
+        _ar2 = GameObject.Find(_ar.selectButton).GetComponent<ar2>();
+        tagSelect = _ar2.GetComponent<ar2>().tag;
+
+        if (_ar.easy) sizeTemp = sizeEasy;
+        else if (_ar.medium) sizeTemp = sizeMedium;
+
+
+        if (value != 0)
+        {
+            if (_ar2._text.text == "")
+                _ar.countSumSize++;
+
+            _ar2._text.text = value.ToString();
+
+            ColorBlock cb = _ar2._button.colors;
+            cb.normalColor = Color.white;
+            _ar2._button.colors = cb;
+
+            string sel = _ar.selectButton;
+            string[] spl = sel.Split(char.Parse(","));
+            col = int.Parse(spl[0]);
+            row = int.Parse(spl[1]);
+
+            ertag = GameObject.FindGameObjectsWithTag(tagSelect);
+            foreach (GameObject er in ertag)
+            {
+                if (er.GetComponent<ar2>()._text.text == value.ToString())
+                    if (er.GetComponent<ar2>().name != sel)
+                    {
+                        tagError[int.Parse(tagSelect) - 1].SetActive(true);
+                        _ar.lockBoard.SetActive(true);
+                    }
+            }
+
+            
+            for (int i = 0; i < sizeTemp; i++)
+            {
+                if (GameObject.Find(col + "," + i.ToString()).GetComponent<ar2>()._text.text == value.ToString())
+                    if (i != row)
+                        if (GameObject.Find(col + "," + i.ToString()).GetComponent<ar2>().tag != tagSelect)
+                        {
+                            colError[col].SetActive(true);
+                            _ar.lockBoard.SetActive(true);
+                        }
+
+                if (GameObject.Find(i.ToString() + "," + row).GetComponent<ar2>()._text.text == value.ToString())
+                    if (i != col)
+                        if (GameObject.Find(i.ToString() + "," + row).GetComponent<ar2>().tag != tagSelect)
+                        {
+                            rowError[row].SetActive(true);
+                            _ar.lockBoard.SetActive(true);
+                        }
+            }
+            
+           
+        }
+
+        if(value == 0)
+        {
+            if (_ar2._text.text != "")
+                _ar.countSumSize--;
+
+            _ar2._text.text = "";
+
+            _ar2._button.colors = _ar2.cb;
+
+            resetError();
+        }
+
+        if (_ar.countSumSize == _ar.countSumMax)  _ar.finish();
+    }
+
+    public void resetError()
+    {
+        for (int i = 0; i < rowError.Length; i++)
+        {
+            rowError[i].SetActive(false);
+            colError[i].SetActive(false);
+            tagError[i].SetActive(false);
+        }
+
+        _ar.lockBoard.SetActive(false);
+    }
+}
